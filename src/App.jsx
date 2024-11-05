8b397dee506a37df89f6ebb4fa59cfca87eb0d44
901990580 <shoxruxahmadjonov776@gmail.com>
2024-11-05 16:33:32 +0500

    tg_bot

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import "./App.css";
 import logo from "./assets/logo 1.jpg";
 import animation from "./assets/animation.gif";
 import Menu from "./Menu";
+import { Button } from "@mui/material";
+// ? import aos
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function App() {
   let navigate = useNavigate();
@@ -11,19 +16,26 @@ function App() {
     navigate("/menu");
   }
 
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
     <div className="App">
       <Routes>
         <Route
           path="/"
           element={
-            <div className="content">
+            <div className="content" data-aos="zoom-in-up">
               <div className="content_head">
                 <h1>Welcome! </h1>
                 <img src={logo} alt="" />
               </div>
               <img className="img" src={animation} alt="" />
-              <button onClick={menuFunc}>Let's Start</button>
+              <Button variant="contained" onClick={menuFunc}>
+                Let's Start
+              </Button>
             </div>
           }
         />
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import menu from "./assets/menu_1.jpg"
+import courses from "./assets/menu_2.jpeg"
+import { IoIosArrowForward } from "react-icons/io";
 
 const Menu = () => {
   return (
     <div className="menu">
       <p>Hello!</p>
-      <h1>Abdulloh</h1>
+      <h1>Shoxrux</h1>
       <div>
-        <p>About us</p>
+        <p>About us <span><IoIosArrowForward /></span></p>
+        
         <img src={menu} alt="" />
       </div>
 
       <div className="">
         <p>Our courses</p>
+        <img src={courses} alt="" />
       </div>
     </div>
   );
